e6caaa6ea6bfc4299944a3fddfdbed58147065f9
Nitesh <nitesh.kumar@arlabhq.in>
2026-02-20 15:16:02 +0530

    Fix: use python3 -m pip instead of pip in start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 set -e
 
 cd backend
-pip install -r requirements.txt
-python manage.py collectstatic --noinput
-python manage.py migrate
-exec gunicorn hrms_backend.wsgi:application --bind 0.0.0.0:${PORT:-8000} --workers 2
+python3 -m pip install -r requirements.txt
+python3 manage.py collectstatic --noinput
+python3 manage.py migrate
+exec python3 -m gunicorn hrms_backend.wsgi:application --bind 0.0.0.0:${PORT:-8000} --workers 2
